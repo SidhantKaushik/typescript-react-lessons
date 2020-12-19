1bf5b6c2aa710c698079dd4976b36ed08e7969a5
Sidhant-Kaushik <sidhant9101@gmail.com>
2020-12-19 10:38:26 -0700

    lesson 6

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Tweet = ({ name, message }: any) => {
+const Tweet = ({ name, tweet, setTweets, tweets }: any) => {
   const deleteTweet = () => {
-    console.log("hel");
+    setTweets(tweets.filter((t: any) => t !== tweet));
   };
   return (
     <div className="Tweet">
       <h2>{name}</h2>
-      <h3>{message}</h3>
+      <h3>{tweet}</h3>
       <button onClick={deleteTweet}>Delete</button>
       <button>Like</button>
     </div>
--- src/components/TweetList.tsx
+++ src/components/TweetList.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Tweet from "./Tweet";
 
-const TweetList = ({ name, tweets }: any) => {
+const TweetList = ({ name, tweets, setTweets }: any) => {
   return (
     <div className="tweet-list">
       {tweets.map((tweet: any) => (
-        <Tweet name={name} message={tweet} />
+        <Tweet
+          name={name}
+          tweet={tweet}
+          setTweets={setTweets}
+          tweets={tweets}
+        />
       ))}
     </div>
   );
